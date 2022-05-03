5b0cdb0f99394a7719dd5fdb6f1b23b842551ff7
Roberto <roberto@opticpower.com>
2022-05-02 21:42:16 -0400

    feat: 🎸 Adding landing page navbar

---
+++ landing/NavBar.js
@@ -0,0 +1,37 @@
+import { Grid } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import SearchIcon from "@mui/icons-material/Search";
+import Image from "next/image";
+
+const useStyles = makeStyles(() => ({
+  container: {
+    background: "#b8635b",
+    height: 60,
+    padding: "0px 20px",
+  },
+  icon: {
+    fontSize: "40px",
+  },
+}));
+
+const NavBar = () => {
+  const classes = useStyles();
+  return (
+    <Grid
+      container
+      alignItems="center"
+      justifyContent="space-between"
+      className={classes.container}
+    >
+      <Image
+        src="/images/logo/Comelon_logo.png"
+        alt="comelon"
+        width="100%"
+        height="50"
+      />
+      <SearchIcon className={classes.icon} />
+    </Grid>
+  );
+};
+
+export default NavBar;
